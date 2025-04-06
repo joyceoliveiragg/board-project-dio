a49ebfa6dfb8f13415988681aad9d8e2fd486d17
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-04-06 02:03:51 -0300

    reparando o codigo

--- src/main/resources/db/changelog/migrations/db.changelog-202408210954.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202408210954.sql
@@ -2,14 +2,14 @@
 --changeset junior:202408191938
 --comment: boards_columns table create
 
-CREATE TABLE BOARDS_COLUMNS(
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    "order" int NOT NULL,
-    kind VARCHAR(7) NOT NULL,
-    board_id BIGINT NOT NULL,
-    CONSTRAINT boards__boards_columns_fk FOREIGN KEY (board_id) REFERENCES BOARDS(id) ON DELETE CASCADE,
-    CONSTRAINT id_order_uk CONSTRAINT unique_board_id_order UNIQUE (board_id, "order")
-)
-
+CREATE TABLE BOARDS_COLUMNS
+(
+    id       BIGSERIAL PRIMARY KEY,
+    name     VARCHAR(255) NOT NULL,
+    "order"  int          NOT NULL,
+    kind     VARCHAR(7)   NOT NULL,
+    board_id BIGINT       NOT NULL,
+    CONSTRAINT boards__boards_columns_fk FOREIGN KEY (board_id) REFERENCES BOARDS (id) ON DELETE CASCADE,
+    CONSTRAINT unique_board_id_order UNIQUE (board_id, "order")
+);
 --rollback DROP TABLE BOARDS_COLUMNS
