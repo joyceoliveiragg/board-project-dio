392847de8ad8390bfe0af5b9da0552cb237a9264
joyceoliveiragg <joyce.ggomes@ufpe.com.br>
2025-04-06 01:47:06 -0300

    reparando o codigo

--- src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
+++ src/main/java/br/com/dio/persistence/config/ConnectionConfig.java
@@ -12,11 +12,19 @@ import static lombok.AccessLevel.PRIVATE;
 public final class ConnectionConfig {
 
     public static Connection getConnection() throws SQLException {
-        var url = "jdbc:postgresql://postgres-dio:5432/dio";
-        var user = "board";
-        var password = "1board23";
+        var url = "jdbc:postgresql://board_postgres:5432/boarddb";
+        var user = "postgres";
+        var password = "postgres";
 
         System.out.println("[DEBUG] Tentando conectar ao banco de dados PostgreSQL...");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new SQLException("Thread de conexão foi interrompida", e);
+        }
+
         var connection = DriverManager.getConnection(url, user, password);
         connection.setAutoCommit(false);
         System.out.println("[DEBUG] Conexão estabelecida com sucesso: " + connection);
--- src/main/resources/db/changelog/migrations/db.changelog-202408281807.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202408281807.sql
@@ -2,6 +2,6 @@
 --changeset junior:202408191938
 --comment: set unblock_reason nullable
 
-ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NULL;
+ALTER TABLE BLOCKS ALTER COLUMN unblock_reason DROP NOT NULL;
 
---rollback ALTER TABLE BLOCKS MODIFY COLUMN unblock_reason VARCHAR(255) NOT NULL;
+--rollback ALTER TABLE BLOCKS ALTER COLUMN unblock_reason SET NOT NULL;
